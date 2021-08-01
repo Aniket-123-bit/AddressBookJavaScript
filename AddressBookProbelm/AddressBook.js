0f210a23d87abaf3adfaeacc3907f4d5c3e6ac4b
Aniket-123-bit <anisunny782@gmail.com>
2021-08-01 23:37:55 +0530

    [Aniket]ADDED code for AddressBookEdit

--- AddressBookProbelm/AddressBook.js
+++ AddressBookProbelm/AddressBook.js
@@ -5,30 +5,35 @@
  * @since: 01.08.2021
  */
 
- const UserMenu = require("./UserMenuData.js"); 
- const AddressBookService = require("./AddressBookServiceClass.js"); 
- 
- let userMenu = new UserMenu();
- let addressBookService = new AddressBookService();
- let personInfoList = new Array();
- flag = true;
- 
- while( flag ) {
- 
-     let option = userMenu.menu();
-     switch( option ) {
-         case 1:
-             personInfoList = addressBookService.insert(personInfoList);
-             break;
-         case 2:
-             console.log(personInfoList);
-             break;
-         case 3:
-             flag = false;
-             break;    
-         default:
-             console.log("You have entered invalid input!");
-             flag = false;
-             break;
-     }
- }
+ const prompt = require('prompt-sync')();
+const UserMenu = require("./UserMenuData.js"); 
+const AddressBookService = require("./AddressBookServiceClass.js"); 
+
+let userMenu = new UserMenu();
+let addressBookService = new AddressBookService();
+let personInfoList = new Array();
+flag = true;
+
+while( flag ) {
+
+    let option = userMenu.menu();
+    switch( option ) {
+        case 1:
+            personInfoList = Array.from(addressBookService.insert(personInfoList));
+            break;
+        case 2:
+            console.log(personInfoList);
+            break;
+        case 3:
+            let fname = prompt("Enter the First Name of the Contact : ");
+            personInfoList = addressBookService.editContact(personInfoList, fname);
+            break; 
+        case 4:
+            flag = false;
+            break;    
+        default:
+            console.log("You have entered invalid input!");
+            flag = false;
+            break;
+    }
+}
---
+++ AddressBookProbelm/AddressBookServiceClass.js
@@ -0,0 +1,80 @@
+//importing another class file to the class file
+const PersonInfo = require("./PersonInfoData.js"); 
+const prompt = require('prompt-sync')();
+
+class AddressBookService {
+    personInfoList = new Array();
+
+    insert(personInfoList) {
+        try{
+            let personInfo = new PersonInfo();
+            personInfo.fName = prompt("Enter First Name : "); 
+            personInfo.lName = prompt("Enter Last Name : "); 
+            personInfo.addressName = prompt("Enter Address : "); 
+            personInfo.cityName = prompt("Enter City : "); 
+            personInfo.stateName = prompt("Enter State : "); 
+            personInfo.zipNumber = prompt("Enter Zip : "); 
+            personInfo.phoneNumberInput = prompt("Enter Phone Number : "); 
+            personInfo.emailInput = prompt("Enter Email : "); 
+
+            personInfoList.push(personInfo);
+
+            return personInfoList;
+        } catch ( e ) {
+            console.error(e);
+        }
+    }
+
+    editContact(personInfoList, fname) {
+        personInfoList.forEach(element => {
+            if( (element.fName === fname) == true ) {
+                console.log("Press 1 to edit Last Name");
+                console.log("Press 2 to edit Address");
+                console.log("Press 3 to edit City")
+                console.log("Press 4 to edit State");
+                console.log("Press 5 to edit Zip");
+                console.log("Press 6 to edit Phone Number");
+                console.log("Press 7 to edit Email");
+                let option = parseInt(prompt("Enter your choice : "));
+                let personInfo = new PersonInfo();
+                
+                switch ( option ) {
+                    case 1:
+                        personInfo.lName = prompt("Enter Last Name : "); 
+                        element.lName = personInfo.lName;
+                        break;
+                    case 2:
+                        personInfo.addressName = prompt("Enter Address : "); 
+                        element.addressName = personInfo.addressName;
+                        break;
+                    case 3:
+                        personInfo.cityName = prompt("Enter City : "); 
+                        element.cityName = personInfo.cityName;
+                        break;
+                    case 4:
+                        personInfo.stateName = prompt("Enter State : "); 
+                        element.stateName = personInfo.stateName;
+                        break;
+                    case 5:
+                        personInfo.zipNumber = prompt("Enter Zip : "); 
+                        element.zipNumber = personInfo.zipNumber;
+                        break;
+                    case 6:
+                        personInfo.phoneNumberInput = prompt("Enter Phone Number : ");
+                        element.phoneNumberInput = personInfo.phoneNumberInput;
+                        break;
+                    case 7:    
+                        personInfo.emailInput = prompt("Enter Email : ");
+                        element.emailInput = personInfo.emailInput;
+                        break;
+                    default:
+                        console.log("You have entered invalid input!");
+                        break;
+                }
+            }
+        });
+        return personInfoList;
+    }
+}
+
+module.exports = AddressBookService;
